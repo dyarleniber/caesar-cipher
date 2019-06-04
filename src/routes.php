eb9e25bcad21636ed6477c70eac69b9a7c0d1340
Dyarlen Iber <dyarlen1@gmail.com>
2019-06-04 00:45:23 -0300

    .env included

--- src/routes.php
+++ src/routes.php
@@ -7,11 +7,19 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-    $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
+    $app->get('/', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
-        $container->get('logger')->info("Slim-Skeleton '/' route");
+        //$container->get('logger')->info("Slim-Skeleton '/' route");
 
         // Render index view
-        return $container->get('renderer')->render($response, 'index.phtml', $args);
+        //return $container->get('renderer')->render($response, 'index.phtml', $args);
+
+        $httpClient = new GuzzleHttp\Client(['base_uri' => getenv('CHALLENGE_BASE_URI')]);
+        $httpResponse = $httpClient->request('GET', getenv('CHALLENGE_GENERATE_URL') . getenv('CHALLENGE_TOKEN'));
+        $httpResponseBody = $httpResponse->getBody();
+
+        $responseArr = json_decode($httpResponseBody, true);
+
+        return $response->withJson($responseArr, 200);
     });
 };
